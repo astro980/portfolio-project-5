b0cbb61665d3736226ad7192ab111845d52813d3
Jurimar Langomez <jurimar.langomez@gmail.com>
2019-10-30 00:51:18 -0600

    Summary Update 1.3v
    Updated
    
    - index.html(Added Div on each links images and Added col class)
    - Roots(html, body)
    -Container(col, container)
    -Gallery(images)
    -Media Queries(768px, 1024 and 1100px)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// -------------------------
+// Lightbox jQuery
+// ------------------------
 lightbox.option({
     'alwaysShowNavOnTouchDevices': true
 });
+
+// -------------------------
+// Type Head js
+// ------------------------
+
+
+
+
+
+function searchIt() {
+    const input = document.getElementById("searchbox");    
+    input.value = searchbox.value.toLowerCase();
+}
+
+document.getElementById("searchbox").addEventListener("keyup", searchIt);
